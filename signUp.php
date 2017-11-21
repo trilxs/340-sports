bd3456c4593b7665af5818e63d64b99446745178
danyollee <leed3@oregonstate.edu>
2017-11-20 22:08:20 -0800

    got signup to work. successfully adds user to the accounts table in the database

--- signUp.php
+++ signUp.php
@@ -1,48 +1,50 @@
-<!-- Register into the database -->
+<!-- Register into database -->
 
 <?php
-  session_start();
+session_start();
   include 'connectvarsEECS.php';
 
-  function generateUserID(){
-    return uniqid();
+  function generateID(){
+    return uniqid(rand());
   }
 
-  function createRandomSalt(){
-    return base64_encode(mcrypt_create_iv(12, MCRYPT_DEV_URANDOM));
+  function generateRandomSalt(){
+     return base64_encode(mcrypt_create_iv(12, MCRYPT_DEV_URANDOM));
   }
 
-  if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($_POST["username"]) || empty($_POST["firstName"]) || empty($_POST["lastName"]) || empty($_POST["email"]) || empty($_POST["age"]) || empty($_POST["password"])){
       $errorMessage = "Please do not leave any field blank.";
       echo $errorMessage;
     }
-  else{
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    if (!$conn) {
-      die('Could not connect: ' . mysql_error());
-    }
-
-    $userID = generateUserID();
-    $currencyAmount = 10000;
-    $username = mysqli_escape_string($conn,$_POST["username"]);
-    $firstName = mysqli_escape_string($conn,$_POST["firstName"]);
-    $lastName = mysqli_escape_string($conn,$_POST["lastName"]);
-    $email = mysqli_escape_string($conn,$_POST["email"]);
-    $age = mysqli_escape_string($conn,$_POST["age"]);
-    $password = mysqli_escape_string($conn,$_POST["password"]);
-    $salt = generateRandomSalt();
-
-    $query = "INSERT INTO accounts(userID, password, email, currencyAmount, username, firstName, lastName, middleName, age, salt) VALUES('$userID', MD5('$password$salt'), '$email', '$currencyAmount' '$username', '$firstName', '$lastName', '$middleName', '$age', '$salt')";
-    if(mysqli_query($conn, $query)){
-      echo "Success, account has been registered";
-    }
     else{
-      echo "Error: could not register. Try again" . mysqli_error($conn);
+      $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+      if (!$conn) {
+        die('Could not connect: ' . mysql_error());
+      }
+
+      $userID = generateID();
+      $currencyAmount = 10000;
+      $username = mysqli_escape_string($conn,$_POST["username"]);
+      $firstName = mysqli_escape_string($conn,$_POST["firstName"]);
+      $lastName = mysqli_escape_string($conn,$_POST["lastName"]);
+      $email = mysqli_escape_string($conn,$_POST["email"]);
+      $age = mysqli_escape_string($conn,$_POST["age"]);
+      $password = mysqli_escape_string($conn,$_POST["password"]);
+      $salt = generateRandomSalt();
+
+      $query = "INSERT INTO accounts(userID, password, email, currencyAmount, username, firstName, lastName, middleName, age, salt) VALUES('$userID', MD5('$password$salt'), '$email', '$currencyAmount', '$username', '$firstName', '$lastName', '$middleName', '$age', '$salt')";
+      if(mysqli_query($conn, $query)){
+        echo "Success, account has been registered";
+      }
+      else{
+        echo "Error: could not register. Try again" . mysqli_error($conn);
+      }
+      mysqli_close($conn);
     }
-    mysqli_close($conn);
   }
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -50,7 +52,7 @@
   <h1>Register an account</h1>
 
   <body>
-    <form action="signUp.php" method="post"><br>
+    <form action="success.php" method="post"><br>
       Username: <input type="text" name="username"><br>
       First name: <input type="text" name="firstName"><br>
       Last name: <input type="text" name="lastName"><br>
